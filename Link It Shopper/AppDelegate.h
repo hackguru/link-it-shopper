c198c885d055db6a93b26ac58d4c41833dc4a8c5
Sina Rezaimehr <edwardmehr@Sinas-Air.home>
2015-02-16 00:50:24 -0800

    Initial Commit

---
+++ Link It Shopper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Link It Shopper
+//
+//  Created by Sina Rezaimehr on 2/16/15.
+//  Copyright (c) 2015 Sina Rezaimehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Link It ShopperTests/Link_It_ShopperTests.m
@@ -0,0 +1,40 @@
+//
+//  Link_It_ShopperTests.m
+//  Link It ShopperTests
+//
+//  Created by Sina Rezaimehr on 2/16/15.
+//  Copyright (c) 2015 Sina Rezaimehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Link_It_ShopperTests : XCTestCase
+
+@end
+
+@implementation Link_It_ShopperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
